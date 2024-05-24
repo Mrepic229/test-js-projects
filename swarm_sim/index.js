c90e1f664089a8879d336b12784e21b296d055ba
David-Pops <dpopowski6371@stu.d214.org>
2024-05-24 11:19:39 -0500

    batman :wq

---
+++ swarm_sim/index.js
@@ -0,0 +1,33 @@
+var number_of_meat = 10;
+
+var number_of_drones = 0; 
+var drone_multiplyer = 1;
+var drone_price = 10;
+
+
+function update() {
+    setInterval(() => {
+        update_drone()
+    }, 1000);
+}
+
+function update_drone(){
+    const number_of_meat_id = document.getElementById("number_of_meat");
+    const number_of_drones_id = document.getElementById("number_of_drones");
+
+    number_of_meat += number_of_drones * drone_multiplyer;
+
+    number_of_meat_id.textContent = number_of_meat
+}
+
+function buy_drone() {
+    const number_of_meat_id = document.getElementById("number_of_meat");
+    const number_of_drones_id = document.getElementById("number_of_drones");
+    number_of_drones += 1;
+    number_of_meat += -drone_price;
+
+    number_of_drones_id.textContent = number_of_drones;
+    number_of_meat_id.textContent = number_of_meat;
+}
+
+update()
