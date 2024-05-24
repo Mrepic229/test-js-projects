f3400299662218e75a265871279f14c3237f422d
David-Pops <dpopowski6371@stu.d214.org>
2024-05-24 10:52:06 -0500

    looping over a console.log()

--- idle_game/index.js
+++ idle_game/index.js
@@ -1,15 +1,46 @@
-function main_click() {
+var money = 1000
+var amount_of_htmls = 0
+var price = 1
+
+function main_button_click() {
     const totalAmountId = document.getElementById("total_amount");
+    const moneyAmountId = document.getElementById("money_amount");
 
-    let amount = Number(totalAmountId.value);
-    console.log(amount);
-   
-    if (isNaN(amount)) {
-        amount = 0;
-    }
-    console.log(amount);
+    money += -price
+    amount_of_htmls += 1
+    console.log(money)
 
-    let result = amount + 1;
-
-    totalAmountId.textContent = result;
+    totalAmountId.textContent = amount_of_htmls;
+    moneyAmountId.textContent = money.toLocaleString(undefined, 
+        {style: "currency",
+        currency: "USD"});
 }
+
+function change_price(amount) {
+    const priceAmoundId = document.getElementById("price_amount");
+    const mainButtonPriceId = document.getElementById("main_button_price");
+
+    if (price + amount < 0) {
+        price = 0;
+    } else { 
+        price += amount; 
+    }
+
+    let result = price;
+
+    priceAmoundId.textContent = result.toLocaleString(undefined, 
+        {style: "currency",
+        currency: "USD"});
+
+    mainButtonPriceId.textContent = price.toLocaleString(undefined, 
+        {style: "currency",
+        currency: "USD"});
+}
+
+function update() {
+    setInterval(() => {
+        console.log(price);
+    }, 1000); // 1000 milliseconds = 1 second
+}
+
+
